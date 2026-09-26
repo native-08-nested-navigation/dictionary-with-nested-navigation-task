2ba7c6e9fd689381636f69bf803b030964556b8b
Artur <arturperetatko30@gmail.com>
2026-09-26 15:24:42 +0300

    Complete task

--- components/LearningNavigation.js
+++ components/LearningNavigation.js
@@ -1,3 +1,39 @@
-function LearningNavigation() {}
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import Statistics from "../screens/Learning/Statistics";
+import Play from "../screens/Learning/Play";
+import { COLORS } from "../constants";
+
+const Drawer = createDrawerNavigator();
+
+function LearningNavigation() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.appBackground,
+        },
+        headerTintColor: COLORS.primary900,
+        headerTitleStyle: {
+          color: COLORS.primary900,
+        },
+        headerTitleAlign: "center",
+        drawerStyle: {
+          backgroundColor: COLORS.primary200,
+        },
+        drawerActiveTintColor: COLORS.primary100,
+        drawerInactiveTintColor: COLORS.fontMain,
+        drawerActiveBackgroundColor: COLORS.primary300,
+        sceneContainerStyle: {
+          backgroundColor: COLORS.appBackground,
+        },
+      }}
+    >
+      <Drawer.Screen name="Statistics" component={Statistics} />
+      <Drawer.Screen name="Play" component={Play} />
+    </Drawer.Navigator>
+  );
+}
 
 export default LearningNavigation;
